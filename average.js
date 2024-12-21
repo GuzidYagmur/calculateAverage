9bea6c8b031b6633bb7081139dc95cebdf356f0e
GuzidYagmur <guzideyagmur@hotmail.com>
2024-12-21 23:30:31 +0300

    average.js

--- average.js
+++ average.js
@@ -1,10 +1,25 @@
 const grades = [50,60,80,90];
-let sum=0;
+
 function calculateAvg(gradesArray){
+    let sum=0;
     for(grade of grades){
          sum += grade;
     }
-    const avg= sum/grades.length;
+    const avg= sum/gradesArray.length;
     return avg;
 }
-console.log(calculateAvg(grades));
+avgGrade = calculateAvg(grades);
+
+if(avgGrade>=90){
+    console.log("You got A");
+}else if(avgGrade>=80 && avgGrade<=89){
+    console.log("You got B");
+}
+else if(avgGrade>=70 && avgGrade<=79){
+    console.log("You got C");
+}
+else if(avgGrade>=60 && avgGrade<=69){
+    console.log("You got D");
+} else {
+    console.log("You got F");
+}
